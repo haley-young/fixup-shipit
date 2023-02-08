856f17aa3deeaa3f1114efc51679b2aedff0ce14
Haley Young <haley.young@shopify.com>
2023-02-08 14:32:10 -0800

    Branch with no fixup commits

---
+++ test.sh
@@ -0,0 +1,7 @@
+        for commit in ["1","2","3", "fixup! test test test"]; do
+          if [[ $commit.message =~ ^fixup!.*$ ]]; then
+            exit 1
+          fi
+        done
+
+        modification
